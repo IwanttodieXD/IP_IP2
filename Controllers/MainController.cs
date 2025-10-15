b5a5a565e91ec479f818c3c2f9201aa651d8dda4
X.X <168252712+IwanttodieXD@users.noreply.github.com>
2025-10-15 12:57:04 +0800

    Add files via upload

---
+++ Controllers/MainController.cs
@@ -0,0 +1,38 @@
+﻿using _10PAGESFKINGSHIT.Models;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _10PAGESFKINGSHIT.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult loadLogin()
+        {
+            return View("~/Views/Main/login.cshtml");
+        }
+        
+        public IActionResult login(login mod)
+        {
+            Random random = new Random();
+            if (mod.username == "admin" && mod.password == "123")
+            {
+                ViewBag.Tries = 0;
+                return View("~/Views/Main/mainDashboard.cshtml");
+            }
+            else
+            {
+                double chance = 0.01;
+                double randnum = random.NextDouble();
+                if (randnum <= chance)
+                {
+                    ViewBag.Message = "try 'admin' and '123' \n sbapn";
+                }
+                else
+                {
+                    ViewBag.Message = "Wrong username and password" + randnum;
+                }
+                return View("~/Views/Main/login.cshtml");
+            }
+        }
+    }
+}
---
+++ Models/login.cs
@@ -0,0 +1,8 @@
+﻿namespace _10PAGESFKINGSHIT.Models
+{
+    public class login
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
